93a4ed3110d7027f0d2bca669a5a6f574056c0e5
Tolu Olaniyan <toluola7@gmail.com>
2018-12-28 23:35:55 +0100

    [feature #162836593] question downvote endpoint

--- server/routes/question.js
+++ server/routes/question.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.post("/", questionsController.saveQuestion);
 router.patch("/:id/upvote", questionsController.questionUpvote);
+router.patch("/:id/downvote", questionsController.questionDownvote);
 
 
 export default router;
