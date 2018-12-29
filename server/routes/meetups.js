05596ccb4600a0700c8e67b9432c49dee26563fa
Tolu Olaniyan <toluola7@gmail.com>
2018-12-29 21:04:59 +0100

    [chore #162870554] protecting some routes

--- server/routes/meetups.js
+++ server/routes/meetups.js
@@ -1,9 +1,10 @@
 import express from "express";
 import meetupsController from "../controllers/meetups";
+import Authenticate from "../helpers/authenticate";
 
 const router = express.Router();
 
-router.post("/", meetupsController.saveMeetup);
+router.post("/", Authenticate.checkAdmin, meetupsController.saveMeetup);
 router.post("/:id/rsvps", meetupsController.saveResponse);
 router.get("/", meetupsController.getMeetups);
 router.get("/:Id", meetupsController.getEachMeetup);
