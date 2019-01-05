fded027c3e98c1946525875f60a30c04d8559eba
Tolu Olaniyan <toluola7@gmail.com>
2019-01-05 21:08:35 +0100

    [chore #162836620] updating readme

--- server/routes/meetups.js
+++ server/routes/meetups.js
@@ -5,10 +5,19 @@ import Validate from "../helpers/validate";
 
 const router = express.Router();
 
-router.post("/", Authenticate.checkAdmin, Validate.validateMeetups, meetupsController.saveMeetup);
-router.post("/:id/rsvps", Validate.validateRes, Validate.validateRespond, meetupsController.saveResponse);
+router.post(
+	"/",
+	Authenticate.checkAdmin,
+	Validate.validateMeetups,
+	meetupsController.saveMeetup
+);
+router.post(
+	"/:id/rsvps",
+	Validate.validateRes,
+	Validate.validateRespond,
+	meetupsController.saveResponse
+);
 router.get("/", meetupsController.getMeetups);
 router.get("/:Id", meetupsController.getEachMeetup);
-
 
 export default router;
