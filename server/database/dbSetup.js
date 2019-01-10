76fe8f6f960aa57c64674718139ca17b3820d02d
Daniel Austin <danielaustin718@gmail.com>
2019-01-10 14:22:33 +0100

    [chore #163112083] setting up the database

---
+++ server/database/dbSetup.js
@@ -0,0 +1,24 @@
+import pg from "pg";
+import dotenv from "dotenv";
+import setupTables from "./dbqueries";
+
+dotenv.config();
+
+const pool = new pg.Pool({
+	connectionString: process.env.DATABASE_URL
+});
+pool.connect((err, client, done) => {
+	if (err) {
+		return `Can not connect to database due to ${err}`;
+	}
+
+	client.query((text, params, callback) => {
+		done();
+		return pool.query(text, params, (error, res) => {
+			callback(error, res);
+		});
+	});
+	return "success";
+});
+
+export default pool;
