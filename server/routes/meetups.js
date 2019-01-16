5b78f9c305920e86d39adee4ca2ce34ed6f903dc
toluola <toluola7@gmail.com>
2019-01-16 12:28:39 +0100

    [chore #163220506] All Routes are authenticated before accessing it

--- server/routes/meetups.js
+++ server/routes/meetups.js
@@ -1,29 +1,34 @@
 import express from "express";
 import meetupsController from "../controllers/meetups";
-import Authenticate from "../helpers/authenticate";
+import authRoute from "../helpers/authenticate";
 import Validate from "../helpers/validate";
 
 const router = express.Router();
 
 router.post(
-	"/",
-	Authenticate.checkAdmin,
-	Validate.validateMeetups,
-	meetupsController.createMeetup
+  "/",
+  authRoute.verifyTokenAdmin,
+  Validate.validateMeetups,
+  meetupsController.createMeetup
 );
 router.post(
-	"/:meetup_id/rsvps",
-	Validate.validateRes,
-	meetupsController.createMeetupResponse
+  "/:meetup_id/rsvps",
+  authRoute.verifyToken,
+  Validate.validateRes,
+  meetupsController.createMeetupResponse
 );
 
 router.get(
-	"/upcomings",
-	// Validate.validateUpcomings,
-	meetupsController.getUpcomingMeetups
+  "/upcomings",
+  authRoute.verifyToken,
+  meetupsController.getUpcomingMeetups
 );
-router.delete("/:id", meetupsController.deleteMeetup);
-router.get("/", meetupsController.getMeetups);
-router.get("/:id", meetupsController.getEachMeetup);
+router.delete(
+  "/:id",
+  authRoute.verifyTokenAdmin,
+  meetupsController.deleteMeetup
+);
+router.get("/", authRoute.verifyToken, meetupsController.getMeetups);
+router.get("/:id", authRoute.verifyToken, meetupsController.getEachMeetup);
 
 export default router;
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,19 +1,10 @@
 import express from "express";
 import userController from "../controllers/user";
-<<<<<<< HEAD
-import Validate from "../helpers/validate";
-
-const router = express.Router();
-
-router.post("/signup", Validate.validateSignup, userController.signup);
-=======
-import validate from "../helpers/helpers"
+import validate from "../helpers/helpers";
 
 const router = express.Router();
 
 router.post("/signup", validate.validateSignup, userController.signup);
 router.post("/login", validate.validateLogin, userController.login);
-
->>>>>>> 871d75ce260b2988cdd05d011987bcd656d7243c
 
 export default router;
