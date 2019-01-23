f95ed459e063619c5a4ee97550053a569649b446
Tolulope olaniyan <39590088+toluola@users.noreply.github.com>
2019-01-23 20:44:43 +0100

    Ft user login form #163400504 (#61)
    * [feature #163400504] User will be able to login using a form

--- UI/js/app.js
+++ UI/js/app.js
@@ -1,45 +1,45 @@
-document.querySelector(".login").addEventListener("click", event => {
-	event.preventDefault();
-	document.getElementById("myForm").style.display = "block";
-})
+// document.querySelector(".login").addEventListener("click", event => {
+// 	event.preventDefault();
+// 	document.getElementById("myForm").style.display = "block";
+// })
 
-document.querySelector(".create").addEventListener("click", event => {
-	event.preventDefault();
-	document.getElementById("myFormSign").style.display = "block";
-})
+// document.querySelector(".create").addEventListener("click", event => {
+// 	event.preventDefault();
+// 	document.getElementById("myFormSign").style.display = "block";
+// })
 
-document.querySelector(".started").addEventListener("click", event => {
-	event.preventDefault();
-	document.getElementById("myFormSign").style.display = "block";
-})
+// document.querySelector(".started").addEventListener("click", event => {
+// 	event.preventDefault();
+// 	document.getElementById("myFormSign").style.display = "block";
+// })
 
-document.querySelector(".account").addEventListener("click", event => {
-	event.preventDefault();
-	document.getElementById("myFormSign").style.display = "block";
-})
+// document.querySelector(".account").addEventListener("click", event => {
+// 	event.preventDefault();
+// 	document.getElementById("myFormSign").style.display = "block";
+// })
 
-document.querySelector(".comment").addEventListener("click", event => {
-	event.preventDefault();
-	document.getElementById("myFormSign").style.display = "block";
-})
+// document.querySelector(".comment").addEventListener("click", event => {
+// 	event.preventDefault();
+// 	document.getElementById("myFormSign").style.display = "block";
+// })
 
-document.querySelector(".ask").addEventListener("click", event => {
-	event.preventDefault();
-	document.getElementById("myFormSign").style.display = "block";
-})
+// document.querySelector(".ask").addEventListener("click", event => {
+// 	event.preventDefault();
+// 	document.getElementById("myFormSign").style.display = "block";
+// })
 
-document.querySelector(".upcoming").addEventListener("click", event => {
-	event.preventDefault();
-	document.getElementById("myFormSign").style.display = "block";
-})
+// document.querySelector(".upcoming").addEventListener("click", event => {
+// 	event.preventDefault();
+// 	document.getElementById("myFormSign").style.display = "block";
+// })
 
-function closeForm() {
-	document.getElementById("myForm").style.display = "none";
-}
+// function closeForm() {
+// 	document.getElementById("myForm").style.display = "none";
+// }
 
-function closeFormSign() {
-	document.getElementById("myFormSign").style.display = "none";
-}
+// function closeFormSign() {
+// 	document.getElementById("myFormSign").style.display = "none";
+// }
 
 
 
